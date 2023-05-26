c0a4d3b32c41bc621982733433af9ea612847329
Marina <marina.abdelmalak2019@gmail.com>
2023-05-26 07:47:59 +0300

    Create retrofit dao and Create Login request model.

--- app/src/main/java/eg/gov/iti/jets/newsapp/util/Constans.kt
+++ app/src/main/java/eg/gov/iti/jets/newsapp/util/Constans.kt
@@ -3,3 +3,4 @@ package eg.gov.iti.jets.newsapp.util
 
 const val BASE_URL = "https://identitytoolkit.googleapis.com/"
 const val SIGNUP_URL = "v1/accounts:signUp"
+const val LOGIN_URL = "v1/accounts:signInWithPassword"
