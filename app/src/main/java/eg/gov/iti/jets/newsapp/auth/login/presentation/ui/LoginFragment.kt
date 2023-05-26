37e050fc4b75eef043d5e43b0ff37874d1623c2a
Marina <marina.abdelmalak2019@gmail.com>
2023-05-26 05:17:34 +0300

    removing commit code

--- app/src/main/java/eg/gov/iti/jets/newsapp/auth/login/presentation/ui/LoginFragment.kt
+++ app/src/main/java/eg/gov/iti/jets/newsapp/auth/login/presentation/ui/LoginFragment.kt
@@ -21,8 +21,7 @@ class LoginFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-       // return inflater.inflate(R.layout.fragment_login, container, false)
+
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
