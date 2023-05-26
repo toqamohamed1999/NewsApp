eee8458af60423399d6ffb9e163d97435ba05001
Marina <marina.abdelmalak2019@gmail.com>
2023-05-26 09:30:09 +0300

    create repo and Implement

--- app/src/main/java/eg/gov/iti/jets/newsapp/util/Constans.kt
+++ app/src/main/java/eg/gov/iti/jets/newsapp/util/Constans.kt
@@ -4,3 +4,5 @@ package eg.gov.iti.jets.newsapp.util
 const val BASE_URL = "https://identitytoolkit.googleapis.com/"
 const val SIGNUP_URL = "v1/accounts:signUp"
 const val LOGIN_URL = "v1/accounts:signInWithPassword"
+const val BASE_URL_NEWS = "https://newsapi.org/"
+const val NEWS ="v2/top-headlines"
