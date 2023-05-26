598c0b350b638bbcd456417b2ec98565e137a5b3
arwa <arwashams643@gmail.com>
2023-05-26 19:00:11 +0300

    feat : fix chide navigation bottom from main

--- app/src/main/java/eg/gov/iti/jets/newsapp/MainActivity.kt
+++ app/src/main/java/eg/gov/iti/jets/newsapp/MainActivity.kt
@@ -10,6 +10,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bottomNavigation: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
