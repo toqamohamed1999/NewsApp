27c238cc6fd62cd20a9b5d47b0d10be21269e8e5
Marina <marina.abdelmalak2019@gmail.com>
2023-05-27 11:18:27 +0300

    create UI favourite list

---
+++ app/src/main/java/eg/gov/iti/jets/newsapp/favourite/presentation/ui/AdapterFavouriteList.kt
@@ -0,0 +1,37 @@
+package eg.gov.iti.jets.newsapp.favourite.presentation.ui
+
+import android.content.ContentValues
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import eg.gov.iti.jets.newsapp.databinding.FragmentFavouriteListBinding
+import eg.gov.iti.jets.newsapp.databinding.RowFavouriteListBinding
+import eg.gov.iti.jets.newsapp.newsscreen.domain.model.Article
+
+class AdapterFavouriteList (private var articalList: List<Article>) :
+    RecyclerView.Adapter<AdapterFavouriteList.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater: LayoutInflater =parent.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        return ViewHolder(RowFavouriteListBinding.inflate(inflater, parent, false))
+    }
+
+    override fun getItemCount(): Int =articalList.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(articalList[position])
+    }
+
+    fun setData(value: List<Article>){
+        this.articalList = value
+
+    }
+    
+    class ViewHolder(private val binding: RowFavouriteListBinding): RecyclerView.ViewHolder(binding.root) {
+        fun bind(article: Article) {
+
+        }
+    }
+}
