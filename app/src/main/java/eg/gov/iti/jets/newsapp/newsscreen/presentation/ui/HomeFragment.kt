0fb906b8254e2085c697212f336db993759b747a
AliSalahElbadry <50172747+AliSalahElbadry@users.noreply.github.com>
2023-05-30 22:40:07 +0300

    Auto Complete

--- app/src/main/java/eg/gov/iti/jets/newsapp/newsscreen/presentation/ui/HomeFragment.kt
+++ app/src/main/java/eg/gov/iti/jets/newsapp/newsscreen/presentation/ui/HomeFragment.kt
@@ -1,17 +1,23 @@
 package eg.gov.iti.jets.newsapp.newsscreen.presentation.ui
 
-import android.R
+import android.annotation.SuppressLint
+import android.app.SearchManager
+import android.database.Cursor
+import android.database.MatrixCursor
 import android.os.Bundle
+import android.provider.BaseColumns
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
+import android.widget.CursorAdapter
+import android.widget.SearchView
+import android.widget.SimpleCursorAdapter
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.facebook.shimmer.ShimmerFrameLayout
+import eg.gov.iti.jets.newsapp.R
 import eg.gov.iti.jets.newsapp.databinding.FragmentHomeBinding
 import eg.gov.iti.jets.newsapp.newsscreen.data.local.ArticleLocalSource
 import eg.gov.iti.jets.newsapp.newsscreen.data.model.NewsResultState
@@ -20,13 +26,15 @@ import eg.gov.iti.jets.newsapp.newsscreen.data.repo.RepoImpl
 import eg.gov.iti.jets.newsapp.newsscreen.domain.model.Article
 import eg.gov.iti.jets.newsapp.newsscreen.presentation.viewmodel.HomeViewModel
 import eg.gov.iti.jets.newsapp.newsscreen.presentation.viewmodel.HomeViewModelFactory
-import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.newFixedThreadPoolContext
 
 
 class HomeFragment : Fragment() {
     private val TAG = "HomeFragment"
-
+    lateinit var searchAdapter: SimpleCursorAdapter
+    private  val from = arrayOf(SearchManager.SUGGEST_COLUMN_TEXT_1)
+    private val to = intArrayOf(R.id.searchItemID)
     private var binding: FragmentHomeBinding? = null
     private lateinit var articleAdapter: ArticleAdapter
     private var articlesList: List<Article> = ArrayList()
@@ -59,14 +67,70 @@ class HomeFragment : Fragment() {
         setUpArticleRecyclerView()
         observeNewsData()
         viewModel.getNews()
+        prepareSearchView()
     }
+private fun prepareSearchView(){
+    searchAdapter = SimpleCursorAdapter(context,
+        R.layout.suggetionlayout,
+        null,
+        from,
+        to,
+        CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER)
+    binding?.searchView?.suggestionsAdapter = searchAdapter
+    binding?.searchView?.setOnQueryTextListener(
+        object: android.widget.SearchView.OnQueryTextListener {
+        override fun onQueryTextSubmit(query: String?): Boolean {
+            viewModel.searchArticles(query)
+            return  true
+        }
+        override fun onQueryTextChange(newText: String?): Boolean {
+            viewModel.search(query =newText)
+            viewModel.searchArticles(newText)
+            return  true
+        }
+    })
+    binding?.searchView?.setOnSuggestionListener(object: SearchView.OnSuggestionListener {
+        override fun onSuggestionSelect(position: Int): Boolean {
+            return false
+        }
 
+        @SuppressLint("Range")
+        override fun onSuggestionClick(position: Int): Boolean {
+            val cursor = binding?.searchView?.suggestionsAdapter?.getItem(position) as Cursor
+            val selection =
+                cursor.getString(cursor.getColumnIndex(SearchManager.SUGGEST_COLUMN_TEXT_1))
+            viewModel.searchArticles(selection)
+            return  true
+        }
+    })
+    lifecycleScope.launch {
+        viewModel.articleSearchState.collect{
+            articlesList = it
+            articleAdapter.submitList(articlesList)
+        }
+    }
+    lifecycleScope.launch{
+        viewModel.stateAutoComplete.collect{
+             val cursor = MatrixCursor(
+                    arrayOf(
+                        BaseColumns._ID,
+                        SearchManager.SUGGEST_COLUMN_TEXT_1
+                    )
+                )
+                it.forEachIndexed { index, s ->
+                        cursor.addRow(arrayOf(index, s))
+                }
+                searchAdapter.changeCursor(cursor)
+        }
+    }
+}
     private fun setUpArticleRecyclerView() {
         articleAdapter = ArticleAdapter()
         articlesList = listOf()
         binding?.articlesRecyclerView?.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = articleAdapter
+
         }
     }
 
